f566c919dc8cc1353515119e4404b0840db1da93
jist <95856749+george0st@users.noreply.github.com>
2023-11-11 21:49:00 +0100

    Doc clarification (new, old solution)

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ if __name__ == '__main__':
 
     args = sys.argv[1:]
     if args[0]=="test":
+        # in-progress new solution with splitting scenarios based on use cases
         test()
     else:
+        # old, stable solution
         sln = qgate.Solution("0-size-100",
                              ["qgate-sln-mlrun-private.env", "qgate-sln-mlrun.env"])
         try:
