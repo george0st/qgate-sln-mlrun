bcaa8e5096b8e65baeb56d4e441d3c3f132f19da
jist <95856749+george0st@users.noreply.github.com>
2023-11-14 19:31:40 +0100

    Add UC301 skelet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import qgate.solution as qgate
 from qgate.nsolution import NSolution
 import os.path
-from qgate.uc import uc101, uc102, uc201
+from qgate.uc import uc101, uc102, uc201, uc301
 from qgate.uc import ucsetup, ucoutput, ucbase
 import sys
 
@@ -12,7 +12,7 @@ def test():
 
     sln=NSolution(setup)
 
-    usecase_fns=[ uc101.UC101, uc201.UC201, uc102.UC102]
+    usecase_fns=[uc101.UC101, uc201.UC201, uc301.UC301, uc102.UC102]
     for usecase_fn in usecase_fns:
         uc=usecase_fn(sln, output)
         try:
