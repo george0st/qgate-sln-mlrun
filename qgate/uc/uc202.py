57bdf9cc5c0633e19f13a107a50747cb89e82bfd
jist <95856749+george0st@users.noreply.github.com>
2023-11-16 16:39:33 +0100

    Add skelet for UC202 (feature vector)

---
+++ qgate/uc/uc202.py
@@ -0,0 +1,22 @@
+"""
+  UC202: Create feature vector(s)
+"""
+
+from qgate.uc.ucbase import UCBase
+from qgate.solution import Solution
+from qgate.uc.ucoutput import UCOutput
+
+
+class UC202(UCBase):
+
+    def __init__(self, sln: Solution, output: UCOutput):
+        super().__init__(sln, output, self.__class__.__name__)
+
+
+    @property
+    def desc(self) -> str:
+        return "Create feature vector(s)"
+
+    def exec(self):
+        self.sln.create_featurevector(self)
+
