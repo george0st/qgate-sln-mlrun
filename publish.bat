9eb7f4fb77bea850ce8e4df50a966b08470b0445
jist <95856749+george0st@users.noreply.github.com>
2023-12-21 10:39:53 +0100

    Add packages for pypi

---
+++ publish.bat
@@ -0,0 +1,13 @@
+rem pip install --upgrade build
+rem pip install --upgrade twine
+
+rmdir /S /Q dist
+rmdir /S /Q build
+rmdir /S /Q qgate_perf.egg-info
+
+rem helper 'https://www.scivision.dev/python-minimal-package/'
+rem https://pypa-build.readthedocs.io/en/latest/
+python -m build --wheel
+
+rem twine upload is supported
+twine upload dist/* --verbose -u__token__
