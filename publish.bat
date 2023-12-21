451d841134c5d52d8a13e651783749ea93b12e69
jist <95856749+george0st@users.noreply.github.com>
2023-12-21 14:24:56 +0100

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -3,7 +3,7 @@ rem pip install --upgrade twine
 
 rmdir /S /Q dist
 rmdir /S /Q build
-rmdir /S /Q qgate.egg-info
+rmdir /S /Q qgate_sln_mlrun.egg-info
 
 rem helper 'https://www.scivision.dev/python-minimal-package/'
 rem https://pypa-build.readthedocs.io/en/latest/
@@ -14,4 +14,4 @@ twine upload dist/* --verbose -u__token__
 
 rmdir /S /Q dist
 rmdir /S /Q build
-rmdir /S /Q qgate.egg-info
+rmdir /S /Q qgate_sln_mlrun.egg-info
