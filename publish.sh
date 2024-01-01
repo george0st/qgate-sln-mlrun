eefe02e3627902f8e0fe834220f13cfb61c01a86
jist <95856749+george0st@users.noreply.github.com>
2024-01-01 19:38:24 +0100

    Repair shell script

--- publish.sh
+++ publish.sh
@@ -3,9 +3,12 @@
 # pip install --upgrade build
 # pip install --upgrade twine
 
-rmdir /S /Q dist
-rmdir /S /Q build
-rmdir /S /Q qgate_sln_mlrun.egg-info
+#rmdir /S /Q dist
+#rmdir /S /Q build
+#rmdir /S /Q qgate_sln_mlrun.egg-info
+rm -rf dist
+rm -rf build
+rm -rf qgate_sln_mlrun.egg-info
 
 # helper 'https://www.scivision.dev/python-minimal-package/'
 # https://pypa-build.readthedocs.io/en/latest/
@@ -14,6 +17,9 @@ python -m build --wheel
 # twine upload is supported
 twine upload dist/* --verbose -u__token__
 
-rmdir /S /Q dist
-rmdir /S /Q build
-rmdir /S /Q qgate_sln_mlrun.egg-info
+#rmdir /S /Q dist
+#rmdir /S /Q build
+#rmdir /S /Q qgate_sln_mlrun.egg-info
+rm -rf dist
+rm -rf build
+rm -rf qgate_sln_mlrun.egg-info
