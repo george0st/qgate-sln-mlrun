b7badd49c678fccf985f269b0dc73a584275df12
jist <95856749+george0st@users.noreply.github.com>
2024-01-13 14:12:58 +0100

    Update ts502.py

--- qgate_sln_mlrun/ts/ts502.py
+++ qgate_sln_mlrun/ts/ts502.py
@@ -48,14 +48,23 @@ class TS502(TSBase):
         vector = fstore.get_feature_vector(f"{project_name}/{featurevector_name}")
 
         #test
-        self._get_data_hint()
+        test_featureset, test_entities, test_features=self._get_test_setting(featurevector_name)
+        test_data=self._get_data_hint(featurevector_name, test_featureset)
 
         with fstore.get_online_feature_service(vector) as svc:
             # TODO add valid party-id from data
             entities = [{"party-id": "d68fe603-7cb1-44e4-9013-7330a050a6be"}]
             resp=svc.get(entities, as_list=True)
 
-    def _get_data_hint(self):
+    def _get_test_setting(self,featurevector_name):
+        test_detail=self.test_setting['vector']['test'][featurevector_name]
+
+        test_featureset=test_detail['feature-set']
+        test_entities=test_detail['entities']
+        test_features=test_detail['features']
+        return test_featureset, test_entities, test_features
+
+    def _get_data_hint(self, featurevector_name, test_featureset):
 
         file = os.path.join(os.getcwd(),
                                    self.setup.model_definition,
@@ -66,5 +75,6 @@ class TS502(TSBase):
             json_content = json.load(json_file)
             name, desc, lbls, kind = TSBase.get_json_header(json_content)
 
-        print(json_content)
+        test_data=json_content['spec']['DataHint-0'][test_featureset]
+        return test_data
 
