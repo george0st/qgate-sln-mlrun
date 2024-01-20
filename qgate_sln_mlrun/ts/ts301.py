fe0589bf3b1a140b1a40c810b167254c8edb6aa9
jist <95856749+george0st@users.noreply.github.com>
2024-01-20 21:47:35 +0100

    Check value with different types

--- qgate_sln_mlrun/ts/ts301.py
+++ qgate_sln_mlrun/ts/ts301.py
@@ -66,6 +66,10 @@ class TS301(TSBase):
                           return_df=False,
                           #infer_options=mlrun.data_types.data_types.InferOptions.Null)
                           infer_options=mlrun.data_types.data_types.InferOptions.default())
+            # TODO: use InferOptions.Null with python 3.10
+            # NOTE: option default, change types
+            # NOTE: option Null, generate error with datetime in python 3.9
+
 
 
 
