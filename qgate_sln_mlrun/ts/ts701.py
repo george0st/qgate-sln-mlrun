20bf7b7a1c3a86193db6e144f469bd18769ee36c
jist <95856749+george0st@users.noreply.github.com>
2024-01-24 22:14:17 +0100

    Create ts701.py

---
+++ qgate_sln_mlrun/ts/ts701.py
@@ -0,0 +1,48 @@
+"""
+  TS601: Serving score from CART
+"""
+
+from qgate_sln_mlrun.ts.tsbase import TSBase
+
+
+class TS601(TSBase):
+
+    def __init__(self, solution):
+        super().__init__(solution, self.__class__.__name__)
+
+    @property
+    def desc(self) -> str:
+        return "Serving score from CART"
+
+    @property
+    def long_desc(self):
+        """
+        Long description, more information see these sources:
+         - https://www.datacamp.com/tutorial/decision-tree-classification-python
+         - https://scikit-learn.org/stable/modules/tree.html
+        """
+        return "Serving score from CART (Classification and Regression Tree) from Scikit-Learn"
+
+    def exec(self):
+        self.serving_score()
+
+    def _build_model(self):
+        # Get off-line data
+
+        # Feature selection
+
+        # Split data
+
+        # Building Decision Tree Model
+        pass
+
+
+    def serving_score(self):
+        """
+        Serve score
+        """
+
+        # Evaluating the Model
+        pass
+
+
