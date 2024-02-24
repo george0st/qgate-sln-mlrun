1d7a0186260102376d706dbce592bb25798a15e4
jist <95856749+george0st@users.noreply.github.com>
2024-02-24 12:52:17 +0100

    Update mlrun-postgres.sh

--- docker/mlrun-postgres.sh
+++ docker/mlrun-postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker pull postgres:12.18
-docker run --name mlrun-postgres -p 8010:8080 -e POSTGRES_PASSWORD=testpwd -d postgres:12.18
+docker run --name mlrun-postgres -p 8010:8080 -e POSTGRES_DB=test POSTGRES_USER=testuser POSTGRES_PASSWORD=testpwd -d postgres:12.18
