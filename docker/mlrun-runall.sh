ce017b338d91c58dd732c14bee99051124bc46cc
jist <95856749+george0st@users.noreply.github.com>
2024-03-05 20:11:01 +0100

    Update mlrun-runall.sh

--- docker/mlrun-runall.sh
+++ docker/mlrun-runall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# create and run all docker containers
+# Create and run all docker containers
 
 # mysql
 docker run --name mlrun-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=jist -e MYSQL_DATABASE=test -e MYSQL_USER=testuser -e MYSQL_PASSWORD=testpwd -d mysql:8.3 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
