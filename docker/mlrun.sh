fc8f44447b9760b55deaa8229d47c0dde9b22ba0
jist <95856749+george0st@users.noreply.github.com>
2024-03-11 11:03:24 +0100

    Update mlrun.sh

--- docker/mlrun.sh
+++ docker/mlrun.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-# mlrun in docker composer
+# MLRun in docker composer
+
+# NOTE:
+#   - YAML file was tailor based on original source
+#   - see https://github.com/mlrun/mlrun/blob/development/docs/install/compose.yaml
+#   - or https://docs.mlrun.org/en/latest/install/local-docker.html#use-mlrun-with-your-own-client
