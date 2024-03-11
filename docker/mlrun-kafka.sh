6724e3b4596f5ee6c34e9f2e8e03f23dc7ceac9e
jist <95856749+george0st@users.noreply.github.com>
2024-03-11 09:50:31 +0100

    Update mlrun-kafka.sh

--- docker/mlrun-kafka.sh
+++ docker/mlrun-kafka.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# single kafka with singl zookeeper
-# NOTE: YAML file was tailor based on original source
-# see https://github.com/conduktor/kafka-stack-docker-compose/blob/master/zk-single-kafka-single.yml
+# Configuration:
+#   - single kafka with single zookeeper (enough for function testiong)
+# NOTE:
+#   - YAML file was tailor based on original source
+#   - see https://github.com/conduktor/kafka-stack-docker-compose/blob/master/zk-single-kafka-single.yml
 
 docker-compose -f ./config/zk-single-kafka-single.yml up -d
