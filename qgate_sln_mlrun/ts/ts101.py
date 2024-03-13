b4309fef60df09e3fa7f1d8c32e355e1226d0d8d
jist <95856749+george0st@users.noreply.github.com>
2024-03-13 19:55:46 +0100

    ...

--- qgate_sln_mlrun/ts/ts101.py
+++ qgate_sln_mlrun/ts/ts101.py
@@ -30,7 +30,7 @@ class TS101(TSBase):
         self.testscenario_new()
         for project_name in self.projects:
             desc = self.project_descs[project_name]
-            self._create_project(f"{project_name}/: '{desc[0]}'", project_name, desc[0], desc[1], desc[2])
+            self._create_project(f"{project_name}/_: '{desc[0]}'", project_name, desc[0], desc[1], desc[2])
 
     @TSBase.handler_testcase
     def _create_project(self, testcase_name, name, desc, lbls, kind):
