98b6c33cecf5a5c17b81cb8d463cb84d3e2552d7
jist <95856749+george0st@users.noreply.github.com>
2024-04-03 10:08:31 +0200

    Repair KafkaTarget (relation to Topic)

--- qgate_sln_mlrun/ts/ts201.py
+++ qgate_sln_mlrun/ts/ts201.py
@@ -189,7 +189,7 @@ class TS201(TSBase):
         elif target == "kafka":
             if self.setup.kafka:
                 params=self.setup.kafka.split(',')
-                target_provider = KafkaTarget(name=target_name, bootstrap_servers=params[0].strip(), topic=params[1].strip())
+                target_provider = KafkaTarget(name=target_name, bootstrap_servers=params[0].strip(), path=params[1].strip())
             else:
                 raise ValueError("Missing value for kafka connection, see 'QGATE_KAFKA'.")
         else:
