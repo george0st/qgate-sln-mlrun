0c0489f2a4849469df83ae23b81f57d1e695db8a
jist <95856749+george0st@users.noreply.github.com>
2024-04-03 22:39:44 +0200

    Update ts502.py

--- qgate_sln_mlrun/ts/ts502.py
+++ qgate_sln_mlrun/ts/ts502.py
@@ -36,8 +36,6 @@ class TS502(TSBase):
             if self.test_setting['vector'].get('online'):
                 vectors = self.test_setting['vector']['online']
 
-
-        # TODO: add bypas for kafka source
         if vectors:
             for project_name in self.projects:
                 for featurevector_name in self.get_featurevectors(self.project_specs.get(project_name)):
