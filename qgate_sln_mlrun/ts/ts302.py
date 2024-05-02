a8570a7660ceffc998124219dc748b6ad2bd4b57
jist <95856749+george0st@users.noreply.github.com>
2024-05-02 09:02:54 +0200

    rename

--- qgate_sln_mlrun/ts/ts302.py
+++ qgate_sln_mlrun/ts/ts302.py
@@ -1,12 +1,12 @@
 """
-  TS302: Ingest data to feature set(s) from CSV Source
+  TS302: Ingest data to feature set(s) from DataFrame Source
 """
 
 from qgate_sln_mlrun.ts.tsbase import TSBase
 import mlrun
 import mlrun.feature_store as fstore
 from mlrun.data_types.data_types import spark_to_value_type
-from mlrun.datastore.sources import CSVSource
+import pandas as pd
 import glob
 import os
 
@@ -18,11 +18,15 @@ class TS302(TSBase):
 
     @property
     def desc(self) -> str:
-        return "Ingest data to feature set(s) from CSV source "
+        return "Ingest data to feature set(s) from DataFrame source"
 
     @property
     def long_desc(self):
-        return "Ingest data to feature set(s) from CSV source"
+        return "Ingest data to feature set(s) from Pandas DataFrame source"
+
+    def prepare(self):
+        """Prepare data for ingestion"""
+        pass
 
     def exec(self, project_name):
         """Data ingest"""
@@ -43,16 +47,19 @@ class TS302(TSBase):
         # get existing feature set (feature set have to be created in previous test scenario)
         featureset = fstore.get_feature_set(f"{project_name}/{featureset_name}")
 
-        fstore.ingest(featureset,
-                      CSVSource(name="tst", path=file),
-                      # overwrite=False,
-                      return_df=False,
-                      #infer_options=mlrun.data_types.data_types.InferOptions.Null)
-                      infer_options=mlrun.data_types.data_types.InferOptions.default())
-        # TODO: use InferOptions.Null with python 3.10 or focus on WSL
-        # NOTE: option default, change types
-        # NOTE: option Null, generate error with datetime in python 3.9
-
-
-
-
+        # ingest data with bundl/chunk
+        for data_frm in pd.read_csv(file,
+                                    sep=self.setup.csv_separator,       #";",
+                                    header="infer",
+                                    decimal=self.setup.csv_decimal,     #",",
+                                    compression="gzip",
+                                    encoding="utf-8",
+                                    chunksize=10000):
+            featureset.ingest(data_frm,
+                          # overwrite=False,
+                          return_df=False,
+                          #infer_options=mlrun.data_types.data_types.InferOptions.Null)
+                          infer_options=mlrun.data_types.data_types.InferOptions.default())
+            # TODO: use InferOptions.Null with python 3.10 or focus on WSL
+            # NOTE: option default, change types
+            # NOTE: option Null, generate error with datetime in python 3.9
--- qgate_sln_mlrun/ts/ts304.py
+++ qgate_sln_mlrun/ts/ts304.py
@@ -1,76 +1,59 @@
 """
-  TS304: Ingest data to feature set(s) from SQL source
+  TS304: Ingest data to feature set(s) from Parquet Source
 """
-
 
 from qgate_sln_mlrun.ts.tsbase import TSBase
 import mlrun
 import mlrun.feature_store as fstore
 from mlrun.data_types.data_types import spark_to_value_type
-from mlrun.datastore.sources import SQLSource
+from mlrun.datastore.sources import ParquetSource
 import pandas as pd
 import glob
 import os
-from qgate_sln_mlrun.mysqlhelper import MySQLHelper
 
 
 class TS304(TSBase):
 
     def __init__(self, solution):
         super().__init__(solution, self.__class__.__name__)
-        self._mysql = MySQLHelper(self.setup)
 
     @property
     def desc(self) -> str:
-        return "Ingest data to feature set(s) from SQL source"
+        return "Ingest data to feature set(s) from Parquet source "
 
     @property
     def long_desc(self):
-        return "Create feature set(s) & Ingest from SQL (MySQL) source"
+        return "Ingest data to feature set(s) from Parquet source"
 
     def exec(self, project_name):
-        """ Create featuresets & ingest"""
-
-        # It can be executed only in case that configuration is fine
-        if not self._mysql.configured:
-            return
-
+        """Data ingest"""
         for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
-            # Create table only in case, that table does not exist
-            if not self._mysql.table_exist(featureset_name):
-                self._mysql.create_table(featureset_name)
-
-            # create file with definition of vector
+            # create possible file for load
             source_file = os.path.join(os.getcwd(),
                                        self.setup.model_definition,
-                                       "01-model",
-                                       "02-feature-set",
-                                       f"*-{featureset_name}.json")
+                                       "02-data",
+                                       self.setup.dataset_name,
+                                       f"*-{featureset_name}.parquet")
 
+            # check existing data set
             for file in glob.glob(source_file):
-                # iterate cross all featureset definitions
-                with open(file, "r") as json_file:
-                    self._create_featureset_ingest(f'{project_name}/{featureset_name}', project_name, featureset_name, json_file)
+                self._ingest_data(f"{project_name}/{featureset_name}", project_name, featureset_name, file)
 
     @TSBase.handler_testcase
-    def _create_featureset_ingest(self, testcase_name, project_name, featureset_name, json_file):
-
+    def _ingest_data(self, testcase_name, project_name, featureset_name, file):
+        # get existing feature set (feature set have to be created in previous test scenario)
         featureset = fstore.get_feature_set(f"{project_name}/{featureset_name}")
 
-        keys = ""
-        for entity in featureset.spec.entities:
-            keys+=f"{entity.name},"
-
         fstore.ingest(featureset,
-                      SQLSource(name="tst",
-                                table_name=self._mysql.convert_feature_tablename(featureset_name),
-                                db_url=self.setup.mysql,
-                                key_field=keys[:-1].replace('-','_')),
+                      ParquetSource(name="tst", path=file),
                       # overwrite=False,
                       return_df=False,
-                      # infer_options=mlrun.data_types.data_types.InferOptions.Null)
+                      #infer_options=mlrun.data_types.data_types.InferOptions.Null)
                       infer_options=mlrun.data_types.data_types.InferOptions.default())
         # TODO: use InferOptions.Null with python 3.10 or focus on WSL
         # NOTE: option default, change types
         # NOTE: option Null, generate error with datetime in python 3.9
+
+
+
 
