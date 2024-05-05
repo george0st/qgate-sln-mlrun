c53e4eb1392a4ddea83735b92e800cc753908561
jist <95856749+george0st@users.noreply.github.com>
2024-05-05 15:35:35 +0200

    ...

--- qgate_sln_mlrun/ts/ts06_pipeline/ts601.py
+++ qgate_sln_mlrun/ts/ts06_pipeline/ts601.py
@@ -28,18 +28,37 @@ class TS601(TSBase):
         """Simple pipeline during ingest"""
 
         self.project_switch(project_name)
-
-        self._ingest_data(f"{project_name}/*", project_name)
+        self._simple_pipeline_plus(f"{project_name}/simple_pipeline_plus", project_name)
+        self._simple_pipeline_multipl(f"{project_name}/simple_pipeline_multipl", project_name)
 
     @TSBase.handler_testcase
-    def _simple_pipeline(self, testcase_name, project_name, featureset_name, file):
+    def _simple_pipeline_plus(self, testcase_name, project_name, featureset_name, file):
 
-        func = mlrun.code_to_function("ts601_function", kind="serving", filename="./ts601_ext_code.py")
+        func = mlrun.code_to_function(f"ts601_{project_name}_fn", kind="serving", filename="./ts601_ext_code.py")
         graph_echo = func.set_topology("flow")
-        graph_echo.to(class_name="TS601Pipeline", name="STEP1", default=True)
+        graph_echo.to(class_name="TS601Pipeline", full_event=True, name="plus", default=True).respond()
 
         # tests
         echo_server = func.to_mock_server(current_function="*")
-        result = echo_server.test("", {"a": 100, "b": 200})
+        result = echo_server.test("", {"a": 5, "b": 7})
         echo_server.wait_for_completion()
-        print(result)
+
+        # value check
+        if result['calc']!=12:
+            raise Exception("Invalid calculation, expected value 12")
+
+    @TSBase.handler_testcase
+    def _simple_pipeline_multipl(self, testcase_name, project_name, featureset_name, file):
+
+        func = mlrun.code_to_function(f"ts601_{project_name}_fn", kind="serving", filename="./ts601_ext_code.py")
+        graph_echo = func.set_topology("flow")
+        graph_echo.to(class_name="TS601Pipeline", full_event=True, name="multipl", default=True).respond()
+
+        # tests
+        echo_server = func.to_mock_server(current_function="*")
+        result = echo_server.test("", {"a": 5, "b": 7})
+        echo_server.wait_for_completion()
+
+        # value check
+        if result['calc']!=35:
+            raise Exception("Invalid calculation, expected value 35")
--- qgate_sln_mlrun/ts/ts06_pipeline/ts601_ext_code.py
+++ qgate_sln_mlrun/ts/ts06_pipeline/ts601_ext_code.py
@@ -1,11 +1,25 @@
 import mlrun
 
+
 class TS601Pipeline:
+
     def __init__(self, context, name=None, **kw):
         self.context = context
         self.name = name
         self.kw = kw
 
-    def do(self, x):
-        print("Echo:", self.name, x)
-        return x
+    def do(self, event):
+        print("Echo:", self.name, event)
+        if self.name.lower()=="multipl":
+            self._multipli(event)
+        elif self.name.lower()=="plus":
+            self._plus(event)
+        return event
+
+    def _multipli(self, event):
+        calc = event.body["a"] * event.body["b"]
+        event.body = {"calc": calc}
+
+    def _plus(self, event):
+        calc = event.body["a"] + event.body["b"]
+        event.body = {"calc": calc}
