0c1be855785488c76ebdcdc3d6b9c36d8b627d0a
jist <95856749+george0st@users.noreply.github.com>
2024-05-20 22:28:59 +0200

    Create ts604.py

---
+++ qgate_sln_mlrun/ts/ts06_pipeline/ts604.py
@@ -0,0 +1,32 @@
+"""
+  TS604: Complex pipeline(s) for ingest
+"""
+
+from qgate_sln_mlrun.ts.tsbase import TSBase
+from qgate_sln_mlrun.setup import Setup
+import mlrun.feature_store as fstore
+import mlrun
+import pandas as pd
+import glob
+import os
+
+
+class TS604(TSBase):
+
+    def __init__(self, solution):
+        super().__init__(solution, self.__class__.__name__)
+
+    @property
+    def desc(self) -> str:
+        return "Complex pipeline(s) for ingest"
+
+    @property
+    def long_desc(self):
+        return "Complex pipeline(s) for ingest"
+
+    def exec(self):
+        return
+
+    @TSBase.handler_testcase
+    def _complex(self, testcase_name, class_call):
+        return
