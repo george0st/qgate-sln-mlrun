2d518a5755af6819bd9eca8569bd43604fa8dcd2
jist <95856749+george0st@users.noreply.github.com>
2024-05-28 21:42:02 +0200

    Update ts401.py

--- qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/ts401.py
+++ qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/ts401.py
@@ -25,17 +25,25 @@ class TS401(TSBase):
 
     def prj_exec(self, project_name):
         """Data ingest"""
-        for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
-            # create possible file for load
-            source_file = os.path.join(os.getcwd(),
-                                       self.setup.model_definition,
-                                       "02-data",
-                                       self.setup.dataset_name,
-                                       f"*-{featureset_name}.csv.gz")
 
-            # check existing data set
-            for file in glob.glob(source_file):
-                self._ingest_data(f"{project_name}/{featureset_name}", project_name, featureset_name, file)
+        pipelines = None
+        if self.test_setting.get('pipeline'):
+            if self.test_setting_pipeline.get('featureset'):
+                pipelines = self.test_setting.pipeline['featureset']
+
+        if pipelines:
+            for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
+                if featureset_name in pipelines:
+                    # create possible file for load
+                    source_file = os.path.join(os.getcwd(),
+                                               self.setup.model_definition,
+                                               "02-data",
+                                               self.setup.dataset_name,
+                                               f"*-{featureset_name}.csv.gz")
+
+                    # check existing data set
+                    for file in glob.glob(source_file):
+                        self._ingest_data(f"{project_name}/{featureset_name}", project_name, featureset_name, file)
 
     @TSBase.handler_testcase
     def _ingest_data(self, testcase_name, project_name, featureset_name, file):
