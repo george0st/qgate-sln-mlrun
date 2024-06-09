fbb9a13706697ed9da5a8176291b18418791a5e5
jist <95856749+george0st@users.noreply.github.com>
2024-06-09 21:14:41 +0200

    Update ts401.py

--- qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/ts401.py
+++ qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/ts401.py
@@ -74,9 +74,12 @@ class TS401(TSBase):
                                                       after=None if not last_step else last_step.name)
 
             if setting.get("MapValues"):
-                last_step = featureset.graph.add_step(fsteps.MapValues(mapping=setting['MapValues']),
+                last_step = featureset.graph.add_step(fsteps.MapValues(mapping=setting['MapValues'],
+                                                                       with_original_features=True),
                                                       name="MapValues",
                                                       after=None if not last_step else last_step.name)
+                #            "MapValues": {"party-familystatus": {"Married": "M", "Single": "S", "Divorced": "D"}},
+
 
             if setting.get("DropFeatures"):
                 last_step = featureset.graph.add_step(fsteps.DropFeatures(features=setting['DropFeatures']),
