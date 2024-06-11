b2d85ffdcaab63aa95d1505ad260db930f0e9880
jist <95856749+george0st@users.noreply.github.com>
2024-06-11 21:33:28 +0200

    Update generateid.py

--- qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/generateid.py
+++ qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/generateid.py
@@ -16,9 +16,11 @@ class GenerateId(StepToDict, MLRunStep):
         super().__init__(**kwargs)
         self.namespace = namespace
         self.features = features
+        self.iterator = 0
 
     def _get_id(self):
-        return uuid.uuid5(self.namespace, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'))
+        self.iterator+=1
+        return uuid.uuid5(self.namespace, f"{self.iterator} {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')}")
 
     def _do_storey(self, event):
         for feature in self.features:
