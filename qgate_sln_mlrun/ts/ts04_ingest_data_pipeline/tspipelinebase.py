903d0d019cfa62bd4120f420e493f6ec3b28a451
jist <95856749+george0st@users.noreply.github.com>
2024-06-12 17:46:28 +0200

    Update tspipelinebase.py

--- qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/tspipelinebase.py
+++ qgate_sln_mlrun/ts/ts04_ingest_data_pipeline/tspipelinebase.py
@@ -9,57 +9,57 @@ class TSPipeline():
     def add_pipeline(self):
         # get existing feature set (feature set have to be created in previous test scenario)
         # add pipelines
-        setting=self.test_setting_pipeline['tests'][featureset_name]
-        if setting:
+
+        if self.setting:
             last_step=None
             # add steps
-            if setting.get("Imputer"):
-                last_step=featureset.graph.add_step(fsteps.Imputer(mapping=setting['Imputer']),
+            if self.setting.get("Imputer"):
+                last_step=self.featureset.graph.add_step(fsteps.Imputer(mapping=self.setting['Imputer']),
                                          name="Imputer",
                                          after=None if not last_step else last_step.name)
 
-            if setting.get("OneHotEncoder"):
-                last_step = featureset.graph.add_step(fsteps.OneHotEncoder(mapping=setting['OneHotEncoder']),
+            if self.setting.get("OneHotEncoder"):
+                last_step = self.featureset.graph.add_step(fsteps.OneHotEncoder(mapping=self.setting['OneHotEncoder']),
                                                       name="OneHotEncoder",
                                                       after=None if not last_step else last_step.name)
 
-            if setting.get("DateExtractor"):
-                last_step = featureset.graph.add_step(fsteps.DateExtractor(parts=setting['DateExtractor']['parts'],
-                                                                           timestamp_col=setting['DateExtractor']['timestamp_col']),
+            if self.setting.get("DateExtractor"):
+                last_step = self.featureset.graph.add_step(fsteps.DateExtractor(parts=self.setting['DateExtractor']['parts'],
+                                                                           timestamp_col=self.setting['DateExtractor']['timestamp_col']),
                                                       name="DateExtractor",
                                                       after=None if not last_step else last_step.name)
 
-            if setting.get("MapValues"):
+            if self.setting.get("MapValues"):
                 # TODO: remove workaround see https://github.com/mlrun/mlrun/issues/5743 (after repair, issue in MLRun 1.6.3)
-                last_step = featureset.graph.add_step(fsteps.MapValues(mapping=setting['MapValues'],
+                last_step = self.featureset.graph.add_step(fsteps.MapValues(mapping=self.setting['MapValues'],
                                                                        with_original_features=True),
                                                       name="MapValues",
                                                       after=None if not last_step else last_step.name)
 
-            if setting.get("DropFeatures"):
-                last_step = featureset.graph.add_step(fsteps.DropFeatures(features=setting['DropFeatures']),
+            if self.setting.get("DropFeatures"):
+                last_step = self.featureset.graph.add_step(fsteps.DropFeatures(features=self.setting['DropFeatures']),
                                                       name="DropFeatures",
                                                       after=None if not last_step else last_step.name)
 
             # own step
-            if setting.get("GenerateId"):
-                last_step = featureset.graph.add_step(generateid.GenerateId(namespace=setting['GenerateId']["namespace"],
-                                                                            features=setting['GenerateId']["features"]),
+            if self.setting.get("GenerateId"):
+                last_step = self.featureset.graph.add_step(generateid.GenerateId(namespace=self.setting['GenerateId']["namespace"],
+                                                                            features=self.setting['GenerateId']["features"]),
                                                       name="GenerateId",
                                                       after=None if not last_step else last_step.name)
 
             # storey steps (works only in engine 'storey', it is valid for engine 'spark' or 'pandas')
-            if setting.get("storey.Filter"):
-                last_step=featureset.graph.add_step("storey.Filter",
+            if self.setting.get("storey.Filter"):
+                last_step=self.featureset.graph.add_step("storey.Filter",
                                      name="storey.Filter",
                                      after=None if not last_step else last_step.name,
-                                     _fn=f"{setting['storey.Filter']}")
+                                     _fn=f"{self.setting['storey.Filter']}")
 
-            if setting.get("storey.Extend"):
-                last_step=featureset.graph.add_step("storey.Extend",
+            if self.setting.get("storey.Extend"):
+                last_step=self.featureset.graph.add_step("storey.Extend",
                                          name="storey.Extend",
                                          after=None if not last_step else last_step.name,
-                                         _fn=f"{setting['storey.Extend']}")
+                                         _fn=f"{self.setting['storey.Extend']}")
 
         # https://docs.mlrun.org/en/latest/feature-store/transformations.html
         #ok - Imputer
@@ -70,15 +70,5 @@ class TSPipeline():
 
         #MLRunStep
 
-        featureset.save()
-
-        # ingest data with bundl/chunk
-        for data_frm in pd.read_csv(file,
-                                    sep=self.setup.csv_separator,
-                                    header="infer",
-                                    decimal=self.setup.csv_decimal,
-                                    compression="gzip",
-                                    encoding="utf-8",
-                                    chunksize=Setup.MIN_BUNDLE):
-            featureset.preview(data_frm)
+        self.featureset.save()
 
