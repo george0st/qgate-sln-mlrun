fe88bdb6cc5391cbdb7e9b5c6413b047fe07ec70
jist <95856749+george0st@users.noreply.github.com>
2024-06-20 21:55:41 +0200

    Update ts305.py

--- qgate_sln_mlrun/ts/ts03_ingest_data/ts305.py
+++ qgate_sln_mlrun/ts/ts03_ingest_data/ts305.py
@@ -37,7 +37,7 @@ class TS305(TSBase):
         for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
             # Create table only in case, that table does not exist
             #if not self._mysql.helper_exist(None, project_name, featureset_name):
-            self._mysql.create_insert_data(project_name, featureset_name, True)
+            self._mysql.create_insert_data(self._mysql.create_helper(project_name, featureset_name), featureset_name, True)
             #self._mysql.create_insert_data(project_name, featureset_name, False)
 
             # create file with definition of vector
