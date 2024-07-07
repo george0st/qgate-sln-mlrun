8015b5ff8309c26d33772b0a05dbe0b54831c48b
jist <95856749+george0st@users.noreply.github.com>
2024-07-07 18:03:13 +0200

    Update ts201.py

--- qgate_sln_mlrun/ts/ts02_feature_set/ts201.py
+++ qgate_sln_mlrun/ts/ts02_feature_set/ts201.py
@@ -13,6 +13,7 @@ class TS201(TSBase):
 
     def __init__(self, solution):
         super().__init__(solution, self.__class__.__name__)
+        self._fs_helper = FeatureSetHelper(self._solution)
 
     @property
     def desc(self) -> str:
@@ -27,24 +28,34 @@ class TS201(TSBase):
         """ Get or create featuresets"""
 
         for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
-            # create file with definition of vector
-            source_file = os.path.join(os.getcwd(),
-                                       self.setup.model_definition,
-                                       "01-model",
-                                       "02-feature-set",
-                                       f"*-{featureset_name}.json")
 
-            for file in glob.glob(source_file):
-                # iterate cross all featureset definitions
-                with open(file, "r") as json_file:
-                    self._create_featureset(f'{project_name}/{featureset_name}', project_name, json_file)
+            definition=self._fs_helper.get_definition(project_name, featureset_name)
+            if definition:
+                self._create_featureset(f'{project_name}/{featureset_name}', project_name, definition)
+
+
+            # # create file with definition of vector
+            # source_file = os.path.join(os.getcwd(),
+            #                            self.setup.model_definition,
+            #                            "01-model",
+            #                            "02-feature-set",
+            #                            f"*-{featureset_name}.json")
+            #
+            # for file in glob.glob(source_file):
+            #     # iterate cross all featureset definitions
+            #     with open(file, "r") as json_file:
+            #         self._create_featureset(f'{project_name}/{featureset_name}', project_name, json_file)
 
     @TSBase.handler_testcase
-    def _create_featureset(self, testcase_name, project_name, json_file):
-        json_content = json.load(json_file)
-        name, desc, lbls, kind = TSBase.get_json_header(json_content)
+    def _create_featureset2(self, testcase_name, project_name, json_file, featureset_prefix):
+        self._fs_helper.create_featureset(project_name, json_file, featureset_prefix)
 
-        if kind == "feature-set":
-            # create feature set
-            fs_helper=FeatureSetHelper(self._solution)
-            fs_helper.create_featureset_content(project_name, name, desc, json_content['spec'])
+    # @TSBase.handler_testcase
+    # def _create_featureset(self, testcase_name, project_name, json_file):
+    #     json_content = json.load(json_file)
+    #     name, desc, lbls, kind = TSBase.get_json_header(json_content)
+    #
+    #     if kind == "feature-set":
+    #         # create feature set
+    #         fs_helper=FeatureSetHelper(self._solution)
+    #         fs_helper.create_featureset_content(project_name, name, desc, json_content['spec'])
