58370871ae619a4bb4efacb82fe632eb2e93e7b6
jist <95856749+george0st@users.noreply.github.com>
2024-07-15 20:29:16 +0200

    Create ts306.py

---
+++ qgate_sln_mlrun/ts/ts03_ingest_data/ts306.py
@@ -0,0 +1,57 @@
+"""
+  TS306: Ingest data to feature set(s) from Kafka source
+"""
+from qgate_sln_mlrun.ts.tsbase import TSBase
+import mlrun
+import mlrun.feature_store as fstore
+from mlrun.data_types.data_types import ValueType
+from mlrun.datastore.sources import KafkaSource
+import json
+from qgate_sln_mlrun.helper.kafkahelper import KafkaHelper
+import os
+import glob
+from qgate_sln_mlrun.helper.featuresethelper import FeatureSetHelper
+
+
+class TS306(TSBase):
+
+    def __init__(self, solution):
+        super().__init__(solution, self.__class__.__name__)
+        self._kafka = KafkaHelper(self.setup)
+
+    @property
+    def desc(self) -> str:
+        return "Ingest data to feature set(s) from Kafka source"
+
+    @property
+    def long_desc(self):
+        return ("Ingest data to feature set(s) from Kafka source")
+
+    def prj_exec(self, project_name):
+        """ Create featuresets & ingest"""
+
+        # It can be executed only in case that configuration is fine
+        if not self._kafka.configured:
+            return
+
+        for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
+            # Create shared topic as data source
+            self._kafka.create_insert_data(self._kafka.create_helper(featureset_name), featureset_name,True)
+            self._create_featureset(f'{project_name}/{featureset_name}', project_name, featureset_name)
+
+    @TSBase.handler_testcase
+    def _create_featureset(self, testcase_name, project_name, featureset_name):
+        # get existing feature set (feature set have to be created in previous test scenario)
+        featureset = fstore.get_feature_set(f"{project_name}/{featureset_name}")
+
+        # samples
+        #  https://github.com/mlrun/test-notebooks/tree/main/kafka_redis_fs
+        #  https://docs.mlrun.org/en/latest/feature-store/sources-targets.html#id1
+
+        # fstore.ingest(featureset,
+        #               KafkaSource(brokers=self.setup.kafka,
+        #                         topics=[self._kafka.create_helper(featureset_name)]),
+        #               # overwrite=False,
+        #               return_df=False,
+        #               # infer_options=mlrun.data_types.data_types.InferOptions.Null)
+        #               infer_options=mlrun.data_types.data_types.InferOptions.default())
