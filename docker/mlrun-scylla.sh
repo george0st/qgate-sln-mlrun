1e8fe6fa9e23f48f3ab8ef956697087c8ba9fc14
jist <95856749+george0st@users.noreply.github.com>
2024-11-01 19:26:50 +0100

    scylla update

--- docker/mlrun-scylla.sh
+++ docker/mlrun-scylla.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker pull scylladb/scylla:6.1.2
-docker run --name mlrun-scylladb -p 9042:9042 -p 7199:7199 -d scylladb/scylla:6.1.2
+docker pull scylladb/scylla:6.2.0
+docker run --name mlrun-scylladb -p 9042:9042 -p 7199:7199 -d scylladb/scylla:6.2.0
