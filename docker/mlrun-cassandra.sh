12d343e8c16932e12930d2c01cdf71fd59b059c2
george0st <steuer.jiri@gmail.com>
2025-08-05 19:33:21 +0200

    switch to cassandra 5.0.5

--- docker/mlrun-cassandra.sh
+++ docker/mlrun-cassandra.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker pull cassandra:5.0.4
-docker run --name mlrun-cassandra -p 9042:9042 -p 7199:7199 -d cassandra:5.0.4
+docker pull cassandra:5.0.5
+docker run --name mlrun-cassandra -p 9042:9042 -p 7199:7199 -d cassandra:5.0.5
 
